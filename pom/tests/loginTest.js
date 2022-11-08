2767e7aa3ccc222e4544229d6242637ff5529805
Carlos Cervantes - vn53fns <Carlos.Cervantes1@walmart.com>
2022-11-07 18:24:00 -0600

    07/11/2022 .env Variables update

--- pom/tests/loginTest.js
+++ pom/tests/loginTest.js
@@ -1,11 +1,13 @@
 import { Selector } from "testcafe";
 import inventoryPage from "../pages/inventoryPage";
 import loginPage from '../pages/loginPage'
+import dotenv from 'dotenv'
+dotenv.config()
 
 fixture`Login test suite`
-    .page`https://www.saucedemo.com/`
+    .page`${process.env.URL_TESTPAGE}`
 
 test('User must be logged in succesfully',async t => {
-    await loginPage.submintLoginForm('standard_user','secret_sauce')
+    await loginPage.submintLoginForm(process.env.USER_PAGE,process.env.PASSWORD)
     await t.expect(inventoryPage.title.exists).ok()
 })
